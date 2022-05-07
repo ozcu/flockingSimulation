3dd3616d45da638b0fc291bed67bd57e95eecc66
ugurhan <ugurhan.buyukozcu@gmail.com>
2022-05-07 14:10:21 +0300

    Add slider texts

--- src/script.js
+++ src/script.js
@@ -3,11 +3,19 @@ flock = [];
 
   function setup(){
 
-    createCanvas(640, 480);
-    
+    createCanvas(windowWidth, windowHeight);
+
+    textSize(16);
+    noStroke();
+
     alignSlider = createSlider(0, 2, 1, 0.1);
+    alignSlider.position(50,800)
+  
     cohesionSlider = createSlider(0, 2, 1, 0.1);
+    cohesionSlider.position(50,850)
+
     separationSlider = createSlider(0, 2, 1, 0.1);
+    separationSlider.position(50,900)
 
     for(let i=0;i<100;i++){
       flock.push(new Boid());
@@ -18,6 +26,13 @@ flock = [];
 
   function draw() {
     background(0);
+
+    text("Align", alignSlider.x * 2 + alignSlider.width, 800);
+    fill(50);
+    text("Cohere", cohesionSlider.x * 2 + cohesionSlider.width,850);
+    fill(50);
+    text("Separate", separationSlider.x * 2 + separationSlider.width, 900);
+    fill(50);
 
     //draw boids
     for(let boid of flock){
